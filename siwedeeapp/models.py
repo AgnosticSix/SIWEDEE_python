e3d03f521471b556358ee602a7a109472d07f034
Abrahamx97 <Abrahamx97@gmail.com>
2016-07-29 19:14:37 -0500

    cambios en los models

--- siwedeeapp/models.py
+++ siwedeeapp/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class ASIGNA_EMPRESA(models.Model):
-    IDASIGNA = models.AutoField(primary_key=True)
-    IDEMPRESA = models.IntegerField(null=True)
-    IDALUMNO = models.IntegerField(null=True)
-    FECHA_VINCU = models.DateField(null=False)
-    FECHA_CAMBIO = models.DateField(null=True)
-
 class CATALUMNO(models.Model):
 	IDALUMNO = models.AutoField(primary_key=True)
 	MATRICULA = models.IntegerField(null=False)
@@ -16,6 +9,9 @@ class CATALUMNO(models.Model):
 	IDCARRERA = models.IntegerField(null=True)
 	IDEMPRESA = models.IntegerField(null=True)
 	ACTIVO = models.BooleanField(null=False)
+
+	def __str__(self):
+		return self.IDALUMNO
 
 class CATCALIFICACIONES(models.Model):
 	IDCALIF = models.AutoField(primary_key=True)
@@ -35,6 +31,9 @@ class CATEMPRESAS(models.Model):
 	TELEFONO = models.BigIntegerField(null=False)
 	DOMICILIO = models.CharField(max_length=120, null=False)
 	EMAIL = models.EmailField(null=False)
+
+	def __str__(self):
+		return self.IDEMPRESA
 
 class CATHISTREP(models.Model):
 	IDALUMNO = models.AutoField(primary_key=True)
@@ -67,9 +66,13 @@ class PROCESO(models.Model):
 	ID_PERIODO = models.IntegerField(null=False)
 	NOMBRE = models.CharField(max_length=25)
 
+class ASIGNA_EMPRESA(models.Model):
+    IDASIGNA = models.AutoField(primary_key=True)
+    IDEMPRESA = models.ForeignKey(CATEMPRESAS)
+    IDALUMNO = models.ForeignKey(CATALUMNO)
+    FECHA_VINCU = models.DateField(null=False)
+    FECHA_CAMBIO = models.DateField(null=True)
+
 class TIPOPERSONA(models.Model):
 	IDTIPOPERSONA = models.AutoField(primary_key=True)
 	NOMBRE = models.CharField(max_length=30, null=False)
-
-	def __str__(self):
-		return self.PASSWORD
